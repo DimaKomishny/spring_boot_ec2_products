75c6dd0df8288e49f2b062a778538a47e0de123b
dimak <dimakomishny@gmail.com>
2022-08-18 06:17:08 +0300

    task_1 added CommandLineRunner Hello world

--- src/main/java/com/dmytro/komyshnyi/ec2/Ec2CrudApplication.java
+++ src/main/java/com/dmytro/komyshnyi/ec2/Ec2CrudApplication.java
@@ -1,12 +1,18 @@
 package com.dmytro.komyshnyi.ec2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ec2CrudApplication {
+public class Ec2CrudApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Ec2CrudApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello world");
     }
 }
